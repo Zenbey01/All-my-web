ca68a4cbaed85c4b8473c34a99261a7006bb0fe2
Panwasa <panwasa.bbey@gmail.com>
2024-06-05 21:26:07 +0700

    update 5/6/24 change and edit navbar

--- shop-react-project/src/components/header.jsx
+++ shop-react-project/src/components/header.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
-function header() {
-  return (
-        <div>
-            <h1>Welcome</h1>
-            <p>to my page</p>
-        </div>
-    )
-}
-
+function Header() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+  
+    return (
+      <nav className="navbar">
+        <div className="navbar-logo">LOGO Soon</div>
+        <ul className="navbar-menu">
+          <li><a href="#">Home</a></li>
+          <li 
+            className="dropdown" 
+            onMouseEnter={() => setDropdownOpen(true)} 
+            onMouseLeave={() => setDropdownOpen(false)}
+          >
+            <a href="#">Product</a>
+            {dropdownOpen && (
+              <ul className="dropdown-menu">
+                <li><a href="#">Category 1</a></li>
+                <li><a href="#">Category 2</a></li>
+                <li><a href="#">Category 3</a></li>
+              </ul>
+            )}
+          </li>
+          <li><a href="#">For sellers</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><button className="sign-in-btn">Sign In</button></li>
+          <li><button className="log-in-btn">Log In</button></li>
+        </ul>
+      </nav>
+    );
+  }
+  
 export default header
